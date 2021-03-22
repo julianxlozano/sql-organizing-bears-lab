5677ae87ffc0bfe569853df427afedc7fad3e7d0
julianxlozano <julianxlozano@gmail.com>
2021-03-22 13:16:44 -0500

    commit tree d3bc128c80c0a677d5e6ae4c003ce0d1e7ebea84

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES('Mr. Chocolate',83,'M','Black','angry',true);
+
+    INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES('Rowdy',11,'M','Black','angry',true);
+
+    INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES('Tabitha',57,'F','Black','angry',true);
+
+    INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES('Sergeant Brown',43,'M','Black','angry',true);
+
+    INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES('Melissa',99,'F','Black','angry',true);
+
+    INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES('Grinch',8,'M','Black','angry',true);
+
+    INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES('Wendy',53,'F','Black','angry',true);
+
+    INSERT INTO bears(name,age,sex,color,temperament,alive)
+    VALUES(NULL,3,'M','Black','angry',true);
